f2c875b78d27d089cb6b882c0c6344984869ca37
emna rahmoune <emnarahmoune13@gmail.com>
2026-05-17 00:36:20 +0100

    fix cors

--- ecom/ecom/src/main/java/com/codeWithProject/ecom/config/CorsConfig.java
+++ ecom/ecom/src/main/java/com/codeWithProject/ecom/config/CorsConfig.java
@@ -7,8 +7,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -21,13 +19,16 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
+
         config.setAllowedOriginPatterns(List.of(
                 "https://front-gules-theta.vercel.app",
                 "http://localhost:4200"
         ));
+
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
         ));
+
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization", "Content-Type"));
 
@@ -35,22 +36,5 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
 
         return source;
-    }
-
-    @Bean
-    public WebMvcConfigurer webMvcCorsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns(
-                                "https://front-gules-theta.vercel.app",
-                                "http://localhost:4200"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
     }
 }
