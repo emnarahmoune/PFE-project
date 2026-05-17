a534c9fb5c73d70c48dd829901754190aa7ad9f9
emna rahmoune <emnarahmoune13@gmail.com>
2026-05-17 17:37:22 +0100

    fix AI endpoints

--- ecom/ecom/src/main/java/com/codeWithProject/ecom/service/impl/FormationAiRecommendationServiceImpl.java
+++ ecom/ecom/src/main/java/com/codeWithProject/ecom/service/impl/FormationAiRecommendationServiceImpl.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class FormationAiRecommendationServiceImpl implements FormationAiRecommendationService {
@@ -20,12 +20,11 @@ private String aiRecommendationUrl;
     @Override
     public FormationRecommendationResponse recommend(FormationRecommendationRequest request) {
         try {
-            FormationRecommendationResponse response = restTemplate.postForObject(
-    aiRecommendationUrl + "/recommend",
-    payload,
-    Map.class
+           FormationRecommendationResponse response = restTemplate.postForObject(
+        aiRecommendationUrl + "/recommend",
+        request,
+        FormationRecommendationResponse.class
 );
-
             if (response == null) {
                 throw new RuntimeException("Le service IA n'a retourné aucune recommandation.");
             }
